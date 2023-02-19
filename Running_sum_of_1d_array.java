00b1c3c0ac6f6145fd231e78a8e6b2779e6defaf
Madhuri122002 <94678055+Madhuri122002@users.noreply.github.com>
2023-02-19 13:49:16 +0530

    Update Running_sum_of_1d_array.java

--- Running_sum_of_1d_array.java
+++ Running_sum_of_1d_array.java
@@ -7,4 +7,17 @@ class Running_sum_of_1d_array
             nums[i]+=nums[i-1];
         return nums;
     }
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter the size of the array:");
+        int size=sc.nextInt();
+        System.out.print("Input:");
+        int nums[]=new int[size];
+        for(int i=0;i<n;i++)
+            nums[i]=sc.nextInt();
+        Running_sum_of_1d_array obj=new Running_sum_of_1d_array();
+        int runningsum[]=obj.runningSum(nums);
+        System.out.println("Output:"+Arrays.toString(nums));
+    }
 }
