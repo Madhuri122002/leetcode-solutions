1283b258921be82ce9dfb4e90062947ebec5f313
Madhuri122002 <94678055+Madhuri122002@users.noreply.github.com>
2023-02-20 20:13:34 +0530

    Create Selection_sort.java

---
+++ Selection_sort.java
@@ -0,0 +1,23 @@
+class Solution 
+{
+    public int[] sortArray(int[] nums)
+    {
+        int min=0, temp;
+        for(int i=0;i<=nums.length-2;i++)
+        {
+            min=i;
+            for(int j=i+1;j<=nums.length-1;j++)
+            {
+                if(nums[j]<nums[min])
+                {
+                    min=j;
+                }
+            }
+            temp=nums[min];
+            nums[min]=nums[i];
+            nums[i]=temp;
+        }
+        return nums;
+
+    }
+}
